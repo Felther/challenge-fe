48246db3a121331056619c9097a3003727f1b92e
Felther <vrocha@pm.me>
2025-03-24 08:59:32 -0300

    Styling the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <section className="bg-blue-50 px-4 py-10">
-      <div>This is the about page</div>
+      <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md m-4 md:m-0">
+        <h2 className="text-3xl text-center font-semibold mb-6">Sobre o aplicativo</h2>
+        <p className="px-6 text-left text-gray-500">Esse app foi feito para o desafio lançado pela Coopercarga para vaga de desenvolvedor jr.</p>       
+      </div>
     </section>
   )
 }
